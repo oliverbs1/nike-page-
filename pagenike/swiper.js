c85f0a0b4098f8db141e33eb00c4cbc646b8befd
oliver bsaibes <oliverbsaibes@gmail.com>
2016-05-08 14:39:52 +0200

    swiper end

---
+++ pagenike/swiper.js
@@ -0,0 +1,7 @@
+   var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 30,
+        freeMode: true
+    });
